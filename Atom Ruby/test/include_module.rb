23a783751797e8f21997ede7309110c171341a18
RobbyDBurns <66960549+RobbyDBurns@users.noreply.github.com>
2020-09-03 11:20:53 -0400

    Added comments for explanation

--- Atom Ruby/test/include_module.rb
+++ Atom Ruby/test/include_module.rb
@@ -1,4 +1,4 @@
-require_relative "Modules.rb"
+require_relative "Modules.rb" # retrieves the Modules.rb file, which allows us to include the module from that file
 include Tools
 Tools.say_hi
 Tools.say_bye
